aa0da543f34b7e8347ece643cf4008c08e533c06
mjan <muhammad.jan@innosight.financial>
2024-12-12 23:03:43 +0500

    fixing error in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-(
-  bootstrapApplication(AppComponent, appConfig) as unknown as Promise<void>
-).catch((err) => console.error(err));
+(async () => {
+  try {
+    await bootstrapApplication(AppComponent, appConfig);
+    console.log('Application bootstrapped successfully!');
+  } catch (err) {
+    console.error('Error bootstrapping the application:', err);
+  }
+})();
